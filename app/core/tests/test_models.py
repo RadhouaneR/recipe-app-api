bbfb8068610f04c0a83bc349f9b6b618f559e080
Radhouane <radhouane@staffery.com>
2023-05-10 11:50:30 +0200

    added recipe image api/ added filtering

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,12 +2,12 @@
 Tests for models
 """
 from decimal import Decimal
+from unittest.mock import patch
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 from core import models
-
 
 def create_user(email='user@example.com', password='testpass123'):
     """Create and return a new user"""
@@ -85,3 +85,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """test generating image path"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
